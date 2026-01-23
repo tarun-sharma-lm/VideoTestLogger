f2a7621c6b0f903a49f0406f93bd14fff951abad
tarunsharma <tarun.sharma@lightmetrics.co>
2026-01-23 14:30:10 +0530

    force stoping and clear package so that it works for 300-400 videos, shorthen the path of files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Create Videos directory and put videos in it.
 
 ## Need to do some changes in Android Studios
-InstrumentationRegistry.getArguments().getString("filename")
-"${rootDirectory}/${filename}"
+val filename = InstrumentationRegistry.getArguments().getString("filename")
+val s3FileUrl = "${rootDirectory}/${filename}"
 
 ## This also creates a text file that lists the name of the vidoes that failed the testcase
--- cellphoneDistraction.sh
+++ cellphoneDistraction.sh
@@ -4,11 +4,11 @@
 CLASSNAME="CellphoneDistractionTest"
 METHODNAME="testVideo"
 # FILENAME="extCap_t_GDWSKYFMAQI7BAAAZ4JIDXRKBA_T1zT0_0014_rPuif.mp4"
-LOG_BASE_PATH="/home/tarunsharma/CellphoneDetectionScript/Logs"
+LOG_BASE_PATH="./Logs"
 # Uploading vidoes to the device
 DEVICE_BASE_PATH="/sdcard/device-test-resources/aiTesting/cellphone"
-LOCAL_BASE_PATH="/home/tarunsharma/CellphoneDetectionScript/Videos"
-FAILEDTESTCASEPATH="/home/tarunsharma/CellphoneDetectionScript/ListOfFailedTestcase.txt"
+LOCAL_BASE_PATH="./Videos"
+FAILEDTESTCASEPATH="./ListOfFailedTestcase.txt"
 DEVICE_LOG_PATH="/data/data/lightmetrics.lib.test/files/lightmetrics"
 
 # clear logs directory 
@@ -26,8 +26,11 @@ fi
 adb logcat -G 512M
 adb shell rm -rf "$DEVICE_BASE_PATH"/*
 adb root
+COUNT=0
+RESTART_INTERVAL=40
 # Create List of FileNames
 for FILE in "$LOCAL_BASE_PATH"/*; do
+    COUNT=$((COUNT + 1))
     FILENAME="$(basename "$FILE")"
     LOGFILENAME="${FILENAME%%.*}.log"
     LOGFILEPATH=$LOG_BASE_PATH/$LOGFILENAME
@@ -50,4 +53,17 @@ for FILE in "$LOCAL_BASE_PATH"/*; do
     rm -rf $LOG_BASE_PATH/lightmetrics
 
     adb shell rm -rf "$DEVICE_BASE_PATH/$FILENAME"
+
+    adb shell am force-stop "lightmetrics.lib.$CLASSNAME"
+
+    if [ $((COUNT % RESTART_INTERVAL)) -eq 0 ]; then
+        echo "Performing maintenance at run $COUNT..."
+        adb shell pm clear "lightmetrics.lib.$CLASSNAME"
+        adb kill-server
+        sleep 2
+        adb start-server
+        sleep 5
+    fi
+    
+    sleep 1
 done
